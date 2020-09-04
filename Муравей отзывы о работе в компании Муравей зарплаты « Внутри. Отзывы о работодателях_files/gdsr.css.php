0751557c9614eb39c7e05489b852a06b134ea300
66exo99 <70640587+66exo99@users.noreply.github.com>
2020-09-04 14:54:56 +0300

    1
    1

---
+++ Муравей отзывы о работе в компании Муравей зарплаты « Внутри. Отзывы о работодателях_files/gdsr.css.php
@@ -0,0 +1,150 @@
+/* stars sizes: 16 */
+/* stars sets: vnutri */
+/* thumbs sizes: 20 */
+/* thumbs sets: vnutri */
+/* loaders: flower, circle,  */
+
+.starsbar.gdsr-size-16, .starsbar.gdsr-size-16 .gdheight, .starsbar.gdsr-size-16 a { height: 16px; }
+.starsbar.gdsr-size-16 a.s1 { width: 16px; }
+.starsbar.gdsr-size-16 a.s2 { width: 32px; }
+.starsbar.gdsr-size-16 a.s3 { width: 48px; }
+.starsbar.gdsr-size-16 a.s4 { width: 64px; }
+.starsbar.gdsr-size-16 a.s5 { width: 80px; }
+.starsbar.gdsr-size-16 a.s6 { width: 96px; }
+.starsbar.gdsr-size-16 a.s7 { width: 112px; }
+.starsbar.gdsr-size-16 a.s8 { width: 128px; }
+.starsbar.gdsr-size-16 a.s9 { width: 144px; }
+.starsbar.gdsr-size-16 a.s10 { width: 160px; }
+.starsbar.gdsr-size-16 a.s11 { width: 176px; }
+.starsbar.gdsr-size-16 a.s12 { width: 192px; }
+.starsbar.gdsr-size-16 a.s13 { width: 208px; }
+.starsbar.gdsr-size-16 a.s14 { width: 224px; }
+.starsbar.gdsr-size-16 a.s15 { width: 240px; }
+.starsbar.gdsr-size-16 a.s16 { width: 256px; }
+.starsbar.gdsr-size-16 a.s17 { width: 272px; }
+.starsbar.gdsr-size-16 a.s18 { width: 288px; }
+.starsbar.gdsr-size-16 a.s19 { width: 304px; }
+.starsbar.gdsr-size-16 a.s20 { width: 320px; }
+.ratepost.gdsr-size-16, .ratepost.gdsr-size-16 .starsbar .gdouter { width: 80px; }
+.rcmmpost.gdsr-size-16, .rcmmpost.gdsr-size-16 .starsbar .gdouter { width: 80px; }
+.rcmmmulti.gdsr-size-16, .rcmmmulti.gdsr-size-16 .starsbar .gdouter { width: 320px; }
+.ratecmm.gdsr-size-16, .ratecmm.gdsr-size-16 .starsbar .gdouter { width: 16px; }
+.reviewcmm.gdsr-size-16, .reviewcmm.gdsr-size-16 .starsbar .gdouter { width: 80px; }
+.gdsr-vnutri .starsbar.gdsr-size-16 .gdouter { background: url('../stars/vnutri/stars16.png') repeat-x 0px 0px; }
+.gdsr-vnutri .starsbar.gdsr-size-16 .gdinner { background: url('../stars/vnutri/stars16.png') repeat-x 0px -32px; }
+.gdsr-vnutri .starsbar.gdsr-size-16 .gdcurrent { background: url('../stars/vnutri/stars16.png') repeat-x 0px -16px; }
+.gdsr-vnutri .starsbar.gdsr-size-16 a:hover { background: url('../stars/vnutri/stars16.png') repeat-x 0px -16px !important; }
+
+.ratepost, .rcmmpost, .ratemulti, .rcmmmulti, .ratecmm, .reviewcmm { position: relative; display: block; }
+.ratepost .starsbar .gdinner, .rcmmpost .starsbar .gdinner, .ratemulti .starsbar .gdinner, .rcmmmulti .starsbar .gdinner, .ratecmm .starsbar .gdinner, .reviewcmm .starsbar .gdinner { width: 0; }
+.ratepost .starsbar a:active, .rcmmpost .starsbar a:active, .ratemulti .starsbar a:active, .rcmmmulti .starsbar a:active, .ratecmm .starsbar a:active, .reviewcmm .starsbar a:active { text-decoration: none; border: 0 !important; }
+.ratepost .starsbar a:visited, .rcmmpost .starsbar a:visited, .ratemulti .starsbar a:visited, .rcmmmulti .starsbar a:visited, .ratecmm .starsbar a:visited, .reviewcmm .starsbar a:visited { text-decoration: none; border: 0 !important; }
+.ratepost .starsbar a:hover, .rcmmpost .starsbar a:hover, .ratemulti .starsbar a:hover, .rcmmmulti .starsbar a:hover, .ratecmm .starsbar a:hover, .reviewcmm .starsbar a:hover { text-decoration: none; border: 0 !important; }
+.ratepost .starsbar a, .rcmmpost .starsbar a, .ratemulti .starsbar a, .rcmmmulti .starsbar a, .ratecmm .starsbar a, .reviewcmm .starsbar a { position: absolute; display: block; left: 0; top: 0; text-decoration: none; border: 0 !important; cursor: pointer; background: none !important; }
+
+.gdt-size-20.gdthumbtext { line-height: 20px; }
+.gdt-size-20.gdthumb, .gdt-size-20.gdthumb a, .gdt-size-20.gdthumb div { width: 20px; height: 20px; }
+.gdt-size-20.gdthumb.gddw a, .gdt-size-20.gdthumb.gddw div { background-position: 0px -20px !important; }
+.gdt-size-20.gdthumb.gdup a:hover { background-position: 0px -40px; }
+.gdt-size-20.gdthumb.gddw a:hover { background-position: 0px -60px !important; }
+.gdt-size-20.gdthumb a.gdt-vnutri, .gdt-size-20.gdthumb div.gdt-vnutri { background: url('../thumbs/vnutri/thumbs20.png') no-repeat; }
+
+.gdthumb a {
+    border: 0 none !important;
+    cursor: pointer;
+    display: block;
+    left: 0;
+    position: absolute;
+    text-decoration: none;
+    top: 0;
+}
+
+.rtexthumb .gdthumbtext { float: none; }
+.gdthumbtext { float: left; font-size: 12px; }
+.gdthumb { position: relative; float: left; }
+.gdthumb.gdup a { background-position:  0px 0px; }
+.thumblock .ratingloader { float: left; }
+
+.ratemulti .starsbar .gdcurrent { width: 0; top: 0; position: absolute; }
+.starsbar .gdinner { padding: 0; }
+.ratingblock td { vertical-align: middle; }
+.raterclear { clear: both; }
+.raterleft { float: left; }
+.raterright { float: right; }
+.voted {color: #999;}
+.inactive {color: #999;}
+.thanks {color: #36AA3D;}
+.static {color: #5D3126;}
+.rater { top: 0; }
+
+.ratingtextmulti { float: left; }
+.ratingbutton { float: right; padding: 1px 6px; }
+.ratingbutton.gdinactive { border: 1px solid #9c5f5f; background-color: #e9e4d4; }
+.ratingbutton.gdactive { border: 1px solid black; background-color: #f1ede5; cursor: pointer; }
+.ratingbutton a { line-height: 14px; text-decoration: none !important; }
+.ratingbutton.gdactive { cursor: pointer; }
+.ratingbutton.gdactive a { color: #ad1b1b; cursor: pointer; }
+.ratingbutton.gdinactive a { color: gray; cursor: default; }
+.gdmultitable { padding: 3px; margin: 3px; border: 1px solid #999999; }
+.gdtblbottom td { padding-top: 4px; }
+.gdtblbottom { margin-top: 2px; background-color: #fffcf4; }
+.mtrow { background-color: #fffcf4; }
+.mtrow td.mtstars { text-align: right; }
+.mtrow.alternate { background-color: #f7f4ea; }
+.gdtblmuravg { background-color: #fffcf4; }
+.gdtblmuravg td { border-top: 2px solid #dcdcdc; text-align: center; }
+.gdmultitable td { vertical-align: middle; padding: 2px 4px; color: black; }
+.ratingblock, .thumblock { margin-bottom: 4px; margin-top: 4px; font-size: 12px; }
+.ratingstarsinline { float: left; }
+.ratingtext { padding-bottom: 2px; margin-bottom: 2px; margin-top: 0px; }
+.ratingmulti img { border: 0; padding: 0; margin: 0; }
+.gdouter { text-align: left; }
+
+.ratingblockarticle { font-size: 1em; }
+.ratingblockcomment { font-size: 0.8em; }
+.ratingloaderarticle, .ratingloadercomment { font-size: 12px; text-align: center; vertical-align: middle; }
+
+.cmminthide { display: none; }
+
+/* loading indicators */
+.loader { margin-left: auto; margin-right: auto; text-align: left; }
+
+.loader.flower { background: url(../gfx/loader/flower.gif) no-repeat left; padding-left: 18px; }
+.loader.flower.width { width: 16px; }
+.loader.circle { background: url(../gfx/loader/circle.gif) no-repeat left; padding-left: 18px; }
+.loader.circle.width { width: 16px; }
+
+.loader.width { padding-left: 0px; }
+.loader.arrows.thumb, .loader.flower.thumb, .loader.circle.thumb, .loader.broken.thumb,
+.loader.radar.thumb, .loader.triangles.thumb, .loader.snake.thumb, .loader.snakebig.thumb,
+.loader.brokenbig.thumb { background-position: center !important; padding-left: 0px !important; }
+
+/* top rating widget */
+.trw-title { text-align: center; font-size: 16px; font-family: "Century Gothic", Arial, Helvetica, sans-serif; }
+.trw-rating { font-size: 44px; font-family: "Century Gothic", Arial, Helvetica, sans-serif; font-weight: bold; text-align: center; }
+.trw-footer { text-align: center; font-size: 11px; font-family: "Century Gothic", Geneva, Arial, Helvetica, sans-serif; }
+
+.gdsrcacheloader {
+    border-top: 1px solid gray;
+    border-bottom: 1px solid gray;
+    margin: 3px;
+    padding: 3px;
+}
+
+.gdsrclsmall {
+    background: url(../gfx/load_small.png) no-repeat left;
+    padding-left: 28px;
+    width: 120px;
+}
+
+.gdsrclbig {
+    background: url(../gfx/load_big.png) no-repeat left;
+    padding-left: 46px;
+    width: 160px;
+}
+
+.gdsrclsmall strong { font-size: 14px; }
+.gdsrclsmall em { font-size: 11px; }
+.gdsrclbig strong { font-size: 17px; }
+.gdsrclbig em { font-size: 14px; }
+
